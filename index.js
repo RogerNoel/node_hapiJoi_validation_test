44d7173ad848c1be4e5cc1aa07268173011a38bf
RogerNoel <noel.roger.pro@gmail.com>
2019-11-09 16:56:40 +0100

    impl. mongoose connection

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const http = require('http');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb:localhost:27017/hapijoi', {useNewUrlParser: true});
+
+const humanSchema = new mongoose.Schema({
+    FirstName: String,
+    lastName: String,
+    age: Number,
+    isMarried: Boolean
+});
+
+const HumanModel = mongoose.model('human', humanSchema);
+
+
 const server = http.createServer((req, res)=>{
     const serverState = res.end('server ON')
     console.log(serverState)
